ac2221e95db5bcd61a8bd3df640bece84989336d
Bryan Vespignani <bryanvespignani@iMac-di-Bryan-2.lan>
2020-09-22 19:21:08 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,202 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1 class="title">Tic Tac Toe</h1>
+    <div class="app">
+      <div v-if="!winner.win" class="turn-container">
+        <h1 class="turn">Turn:</h1>
+        <h1 class="symbol-view">{{ turn ? "X" : "O" }}</h1>
+      </div>
+      <div v-if="winner.win" class="turn-container">
+        <h1 class="turn">{{ winner.player }} ha vinto!</h1>
+      </div>
+      <div class="game-container">
+        <div v-for="(cell, i) in cells" :key="i">
+          <Cell
+            :class="{ 'winning-cell': winningCells.includes(i) }"
+            @nextTurn="changeTurn($event)"
+            :turn="turn"
+            :index="i"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Cell from "./components/Cell.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Cell
+  },
+  data: () => ({
+    cells: Array.from({ length: 9 }),
+    winningCells: [],
+    turn: true,
+    winner: {
+      win: false,
+      player: "",
+      playerBool: false
+    }
+  }),
+  methods: {
+    changeTurn(index) {
+      /*
+        turn:
+          true  -> X
+          false -> O
+      */
+      this.cells[index] = this.turn;
+      const winner = this.checkForWinner();
+      if (winner !== -1) {
+        console.log(winner + " ha won!");
+        this.winner.win = true;
+        this.winner.player = winner ? "X" : "O";
+      }
+      this.turn = !this.turn;
+    },
+    /**
+     * Check if al element of a given set are equal.
+     * Just checking if size is 1. Also check if the set contains
+     * an undefined element.
+     * @param {Set} elm
+     * @returns {(number|string)} returns -1 if none won; in case of win
+     *                            returns a string containing the winner
+     */
+    allEqual(elm) {
+      if (elm.size === 1 && !elm.has(undefined)) {
+        return elm.values().next().value;
+      }
+      return -1;
+    },
+    checkForWinner() {
+      // Rows
+      for (let i = 0; i < 9; i += 3) {
+        // row contains a single row of the matrix
+        let indexes = [i, i + 1, i + 2];
+        const row = new Set(this.cells.slice(i, i + 3));
+        const equal = this.allEqual(row);
+        if (equal !== -1) {
+          this.winningCells = [...indexes];
+          return equal;
+        }
+      }
+
+      // Cols
+      for (let i = 0; i < 3; i++) {
+        // col is a set which contains a column
+        // i -> 0, 1, 2 is the number of column
+        // j -> is the index of a cells; the filter returns:
+        // return i + 3 === j || i + 6 === j || j === i
+
+        let indexes = [i, i + 3, i + 6];
+        const col = new Set(
+          this.cells.filter((_, j) => {
+            let elm = false;
+            for (let k = 0; k < 3; k++) {
+              elm = elm || i + 3 * k === j;
+            }
+            return elm;
+          })
+        );
+
+        const equal = this.allEqual(col);
+        if (equal !== -1) {
+          this.winningCells = [...indexes];
+          return equal;
+        }
+      }
+
+      // Main diagonal
+      let indexes = [0, 4, 8];
+      const diag = new Set(
+        indexes.map(i => {
+          return this.cells[i];
+        })
+      );
+
+      let equal = this.allEqual(diag);
+      if (equal !== -1) {
+        this.winningCells = [...indexes];
+        return equal;
+      }
+
+      // Counterdiagonal
+      indexes = [2, 4, 6];
+      const cdiag = new Set(
+        indexes.map(i => {
+          return this.cells[i];
+        })
+      );
+
+      equal = this.allEqual(cdiag);
+      if (equal !== -1) {
+        this.winningCells = [...indexes];
+        return equal;
+      }
+
+      return -1;
+    }
   }
 };
 </script>
 
 <style lang="scss">
-#app {
+body {
+  background: whitesmoke;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+
+.title {
+  font-size: 5em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0em;
+  color: #ff595e;
+}
+
+.app {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+.game-container {
+  display: grid;
+  grid-template-columns: repeat(3, 0.1fr);
+  gap: 0;
+  width: min-content;
+  border-radius: 10px;
+  padding: 1rem;
+  margin: 0;
+  box-shadow: 0px 13px 28px -10px rgba(0, 0, 0, 0.416);
+}
+
+.turn-container {
+  display: inline-flex;
+  align-items: center;
+}
+
+.turn {
+  font-size: 5em;
+}
+
+.cross {
+  color: #1982c4;
+}
+
+.circle {
+  color: #8ac926;
+}
+
+.symbol-view {
+  font-size: 5em;
+  width: 1em;
+  margin-left: 0.8rem;
+}
+
+.winning-cell {
+  background: #ffca3a;
 }
 </style>
---
+++ src/components/Cell.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="cell" @click="changeState">
+    <!-- <span>{{ symbols[state] }}</span> -->
+    <img class="shadow" :src="symbols[state]" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    symbols: [
+      "",
+      require("@/assets/cross.svg"),
+      require("@/assets/circle.svg")
+    ],
+    state: 0,
+    enable: true
+  }),
+  computed: {},
+  props: {
+    index: Number,
+    turn: Boolean
+  },
+  methods: {
+    changeState() {
+      if (this.enable) {
+        this.enable = false;
+        this.state = this.turn ? 1 : 2;
+        this.$emit("nextTurn", this.index);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.cell {
+  border: 1px solid black;
+  width: 15vw;
+  height: 15vw;
+  text-align: center;
+  font-size: 8vw;
+  background: rgb(255, 255, 255);
+}
+
+.cell:hover {
+  opacity: 0.8;
+  cursor: pointer;
+}
+
+img {
+  transform: scale(0.7);
+}
+
+.shadow {
+  filter: drop-shadow(3px 3px 2px rgba(0, 0, 0, 0.7));
+}
+</style>
